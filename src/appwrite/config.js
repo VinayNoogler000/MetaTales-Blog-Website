abd34702cd33f8625def1b957a3193dc59692ce7
VinayNoogler000 <vinaytambey000@gmail.com>
2025-12-26 20:25:23 +0530

    Add a functionality as a service to perform CRUD ops. on Posts.

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,2 +1,34 @@
 import envConfig from "../envConfig";
-import { Client, Account, ID } from "appwrite";
+import { Client, ID, Storage, Query, TablesDB } from "appwrite";
+
+export class Service{
+    client = new Client();
+    tablesDB;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(envConfig.appwriteUrl)
+            .setProject(envConfig.appwriteProjectId);
+        this.tablesDB = new TablesDB(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}) {
+        try {
+            return await this.tablesDB.createRow({
+                databaseId: envConfig.appwriteDBId,
+                tableId: envConfig.appwriteCollectionId,
+                rowId: slug,
+                data: {title, content, featuredImage, status, userId}
+            });
+        }
+        catch(err) {
+            console.error("Appwrite/Cofig.js :: createPost() :: error", err);
+        }
+    }
+}
+
+const service = new Service();
+
+export default service;

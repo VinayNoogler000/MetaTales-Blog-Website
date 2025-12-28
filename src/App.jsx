45723e8ac6564f8cf3cb49f9adce0eb40f81a771
VinayNoogler000 <vinaytambey000@gmail.com>
2025-12-28 19:45:54 +0530

    Implement user authentication and layout structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <h1>MetaTales</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else {
+          dispatch(logout());
+        }
+      })
+      .catch(err => console.error("Unable to Get Curr User Status"))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return loading ? null : (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full text-black'>
+        <Header/>
+        <main>
+          TODO: <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div >
   )
 }
 

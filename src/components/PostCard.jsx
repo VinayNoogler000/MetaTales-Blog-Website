e6c6091d141e32cb46508503a9fb161f7b93e187
VinayNoogler000 <vinaytambey000@gmail.com>
2026-01-21 17:07:03 +0530

    Create PostCard component for displaying blog post previews

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import postService from "../appwrite/post";
+import imageService from "../appwrite/image";
+import {Link} from "react-router-dom";
+
+function PostCard({$id, title, featuredImage}) {
+    return (
+        <Link to={`/post/${$id}`}>
+            <div className="w-full bg-gray-100 rounded-xl p-4">
+                <div className="w-full justify-center mb-4">
+                    <img src={imageService.getImagePreview(featuredImage)} alt={title} className="rounded-xl"/>
+                </div>
+            </div>
+
+            <h2 className="text-xl font-bold">{title}</h2>
+        </Link>
+    );
+}

ca40014988cb2a950095ec5818ee4eeec0bc08fc
VinayNoogler000 <vinaytambey000@gmail.com>
2026-01-23 13:20:58 +0530

    Refactor main.jsx to implement routing with AuthLayout for authentication handling

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,53 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from "./store/store.js"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AddPost, AllPosts, EditPost, Home, Login, Post, Signup } from "./pages"
+import { AuthLayout } from './components/index.js'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/login',
+        element: (<AuthLayout authenticated={false}> <Login/> </AuthLayout>)
+      },
+      {
+        path: "/signup",
+        element: (<AuthLayout authenticated={false}> <Signup/> </AuthLayout>)
+      },
+      {
+        path: "/all-posts",
+        element: (<AuthLayout authenticated={true}> <AllPosts/> </AuthLayout>)
+      },
+      {
+        path: "/add-post",
+        element: (<AuthLayout authenticated={true}> <AddPost/> </AuthLayout>)
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (<AuthLayout authenticated={true}> <EditPost/> </AuthLayout>)
+      },
+      {
+        path: "/post/:slug",
+        element: (<AuthLayout authenticated={true}> <Post/> </AuthLayout>)
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
     </Provider>
   </StrictMode>,
 )

35124952ca4da8c14e1cebad452b69167310444d
VinayNoogler000 <vinaytambey000@gmail.com>
2026-01-23 12:49:09 +0530

    Add index.js to export page components: Home, Signup, Login, AllPosts, Post, AddPost, and EditPost

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Signup from "./Signup";
+import Login from "./Login";
+import AllPosts from "./AllPosts";
+import Post from "./Post";
+import AddPost from "./AddPost";
+import EditPost from "./EditPost";
+
+export {Home, Signup, Login, AllPosts, Post, AddPost, EditPost };

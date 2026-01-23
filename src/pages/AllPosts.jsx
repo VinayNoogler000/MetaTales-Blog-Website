991339100ce6c9de42618c6720c0aca4b2a63b75
VinayNoogler000 <vinaytambey000@gmail.com>
2026-01-23 12:43:55 +0530

    Add new page components: AddPost, AllPosts, EditPost, Home, Login, Post, and Signup

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { postService } from '../appwrite'
+import { Container, PostCard } from '../components'
+
+function AllPosts() {
+    const [posts, setPosts] = React.useState([]);
+
+    React.useEffect(() => { }, []);
+
+    postService.getPosts().then(posts => {
+        if (posts) {
+            setPosts(posts.documents);
+        }
+    });
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard post={post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default AllPosts
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, PostForm } from '../components';
+import { postService } from '../appwrite';
+import { useParams, useNavigate } from 'react-router-dom';
+
+export default function EditPost() {
+    const [post, setPost] = React.useState(null);
+    const {slug} = useParams();
+    const navigate = useNavigate();
+
+    React.useEffect(() => {
+        console.log("Hello World");
+        
+        if (slug) {
+            postService.getPost(slug).then(post => {
+                if (post) {
+                    setPost(post);
+                }
+            })
+        }
+        else {
+            navigate('/');
+        }
+    }, [slug, navigate]);
+
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+    ) : null;
+}

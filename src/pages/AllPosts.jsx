83b72bcaa9a775680709fc028845446e2d0d28d5
VinayNoogler000 <vinaytambey000@gmail.com>
2026-01-24 19:57:47 +0530

    Add conditional check for posts in AllPosts component to prevent errors

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -17,7 +17,7 @@ function AllPosts() {
         <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
-                    {posts.map((post) => (
+                    {posts && posts.map((post) => (
                         <div key={post.$id} className='p-2 w-1/4'>
                             <PostCard post={post} />
                         </div>

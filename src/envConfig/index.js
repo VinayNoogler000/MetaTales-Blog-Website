979aa6beefd7187eb0663c84b18cfc759daa5784
VinayNoogler000 <vinaytambey000@gmail.com>
2026-01-24 20:19:44 +0530

    Add TinyMCE API key to envConfig and update RTE component to use it

--- src/envConfig/index.js
+++ src/envConfig/index.js
@@ -5,6 +5,7 @@ const envConfig = {
     appwriteDBId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinyMceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY)
 }
 
 export default envConfig;
